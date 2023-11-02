c2504480ea635c2205f84be0aefece51d055c968
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2023-11-02 22:17:23 +0100

    feat(style): animation for button

--- src/components/global/Button.ts
+++ src/components/global/Button.ts
@@ -8,6 +8,7 @@ export class SimpleButton extends Component {
     static styles = css`
       :host {
         width: 100%;
+        --delay: .85s
       }
 
       div {
@@ -17,20 +18,58 @@ export class SimpleButton extends Component {
       }
 
       a {
+        position: relative;
+        z-index: 2;
         ${textColor};
         text-decoration: none;
         outline: var(--color-light) solid 3px;
         padding: 0.5em;
-        transition: .3s;
         text-transform: uppercase;
         font-weight: bold;
         transform: scale(1, 1.1);
+        transition: .3s;
+        transition-delay: var(--delay);
       }
 
       a:hover {
-        background: #e1e1e1;
-        color: black;
-        transition: .5s;
+        color: var(--color-dark);
+        transition: .3s;
+        transition-delay: var(--delay);
+      }
+      span {
+        position: absolute;
+        display: block;
+        height: 100%;
+        width: 0;
+        inset: 0;
+        z-index: -1;
+        box-sizing: content-box;
+        background: var(--color-light);
+        animation: off 1s ease-in-out;
+        animation-fill-mode: forwards;
+      }
+      a:hover span {
+        animation: on 1s ease-in-out;
+        animation-fill-mode: forwards;
+      }
+      
+      @keyframes on {
+        from {
+          width: 0;
+        }
+        to {
+          width: 100%;
+        }
+      }
+      @keyframes off {
+        from {
+          left: 0;
+          width: 100%;
+        }
+        to {
+          left: 100%;
+          width: 0;
+        }
       }
     `
 
@@ -41,7 +80,10 @@ export class SimpleButton extends Component {
         const content = this.innerHTML
         return html`
             <div>
-                <a href="${this.href}">${content}</a>
+                <a href="${this.href}">
+                    ${content}
+                    <span></span>
+                </a>
             </div>
         `
     }
