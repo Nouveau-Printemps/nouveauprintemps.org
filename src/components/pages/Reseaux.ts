a77aa49c0c6dce68d41ae640391b22023e861c05
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2024-02-14 14:51:38 +0100

    feat(reseaux): rewrite some sections

--- src/components/pages/Reseaux.ts
+++ src/components/pages/Reseaux.ts
@@ -29,7 +29,13 @@ export class Reseaux extends Component {
                 </p>
                 <p>
                     Nous avons quitté Twitter, car nous sommes fondamentalement contre la vision qu'Elon Musk a pour ce 
-                    réseau. 
+                    réseau.
+                </p>
+                <p>
+                    Nous avons aussi lancé notre propre instance Mastodon : le Nouveau Printemps Social. 
+                    L'annonce est disponible 
+                    <a href="https://blog.nouveauprintemps.org/le-reseau-social-du-nouveau-printemps/" target="_blank">
+                        ici</a>.
                 </p>
             </g-pib>
             <g-pib title="Blog" color="green" img="/icons/logo.svg"
@@ -45,8 +51,7 @@ export class Reseaux extends Component {
             <g-pib title="YouTube" color="dark" left img="/icons/brands/youtube.svg">
                 <p>
                     YouTube est l'un de deux moyens de diffusion vidéo que nous utilisons.
-                    Toutes les vidéos importantes y seront diffusées, même si, certaines fois, elles seront censurées pour
-                    éviter de se faire bannir.
+                    Toutes les vidéos importantes y seront diffusées.
                 </p>
                 <p>
                     Le contenu posté sera essentiellement constitué de présentations, de trailers et de "clip".
@@ -55,10 +60,9 @@ export class Reseaux extends Component {
             <g-pib title="PeerTube" color="green" img="/icons/brands/peertube.svg">
                 <p>
                     Deuxième manière de diffusion vidéo que nous utilisons, PeerTube est l'alternative libre de YouTube.
-                    Le principal avantage de PeerTube est l'absence de censure politique nous concernant.
                 </p>
                 <p>
-                    Tout le contenu vidéo que nous produisons sera posté sans censure sur PeerTube.
+                    Tout le contenu vidéo que nous produisons sera posté sur PeerTube.
                 </p>
             </g-pib>
             <g-footer color="light"></g-footer>
